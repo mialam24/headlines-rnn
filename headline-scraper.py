1f1e0bd7ae0647bce8249e96d4653c2f4097fb99
Ismail Alam <ismail.alam2@gmail.com>
2017-12-15 00:50:49 -0800

    loops from one date to another

--- headline-scraper.py
+++ headline-scraper.py
@@ -1,23 +1,28 @@
 # Adapted from https://stackoverflow.com/questions/43750044/google-news-crawler-to-return-results-with-url-title-and-briefing
 
 import time
-from random import randint
+import datetime
 import requests
 from bs4 import BeautifulSoup
 
-subject = "bitcoin"
-month_s = str(12)
-day_s = str(14)
-year_s = str(14)
-month_e = str(2017)
-day_e = str(14)
-year_e = str(2017)
-url = "https://www.google.com.sg/search?q=%22"+subject+"%22&source=lnt&tbs=cdr%3A1%2Ccd_min%3A"+month_s+"%2F"+day_s+"%2F"+year_s+"%2Ccd_max%3A"+month_e+"%2F"+day_e+"%2F"+year_e+"%2&tbm=nws"
-time.sleep(randint(0, 2))
-htmlpage = requests.get(url)
-soup = BeautifulSoup(htmlpage.text,'lxml')
-for result_table in soup.findAll("div", {"class": "g"}):
-    a_click = result_table.find("a")
-    raw_title = str(a_click.renderContents())
-    proc_title = ((raw_title[2:])[:-1]).replace('<b>','').replace('</b>','')
-    print(proc_title)
+def print_headlines(subject, 
+                    year_s, month_s, day_s, 
+                    year_e, month_e, day_e):
+    start_date = datetime.datetime(year_s, month_s, day_s)
+    end_date = datetime.datetime(year_e, month_e, day_e)
+
+    curr_date = start_date
+    while curr_date <= end_date:
+        print(curr_date.strftime('%Y-%m-%d'))
+        url = "https://www.google.com.sg/search?q=%22"+subject+"%22&tbs=cdr%3A1%2Ccd_min%3A"+str(curr_date.month)+"%2F"+str(curr_date.day)+"%2F"+str(curr_date.year)+"%2Ccd_max%3A"+str(curr_date.month)+"%2F"+str(curr_date.day)+"%2F"+str(curr_date.year)+"%2&tbm=nws"
+        htmlpage = requests.get(url)
+        soup = BeautifulSoup(htmlpage.text,'lxml')
+        for result_table in soup.findAll("div", {"class": "g"}):
+            a_click = result_table.find("a")
+            raw_title = str(a_click.renderContents())
+            proc_title = ((raw_title[2:])[:-1]).replace('<b>','').replace('</b>','')
+            print(proc_title)
+        curr_date += datetime.timedelta(days=1)
+        time.sleep(2)
+
+print_headlines("bitcoin", 2017, 12, 12, 2017, 12, 14)
