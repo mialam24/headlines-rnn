f1f0529d5d6c861ee54bdcec11432f2893575470
Ismail Alam <malam1@illumina.com>
2017-12-16 13:33:38 -0800

    added padding so each line is 100 chars

--- commands.sh
+++ commands.sh
@@ -9,4 +9,4 @@ ruby nytimes-article-api.rb $API_KEY google 20000101 20171215 200
 
 #####
 
-python train.py --batch_size 200 --seq_length 200 --num_epoch 400 --learning_rate 0.001 --data_dir=../headlines-rnn/data/ # APPLE
+python train.py --rnn_size 256 --batch_size 200 --seq_length 200 --num_epoch 400 --learning_rate 0.001 --data_dir=../headlines-rnn/data/ # APPLE
