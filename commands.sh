36d756e93f23809eec7d7ab4ff9e66aaad9f5afe
Ismail Alam <malam1@illumina.com>
2017-12-16 20:21:32 -0800

    added param for apple-0

--- commands.sh
+++ commands.sh
@@ -6,7 +6,4 @@ ruby nytimes-article-api.rb $API_KEY apple 20000101 20171215 200
 ruby nytimes-article-api.rb $API_KEY apple 20130614 20171215 200
 ruby nytimes-article-api.rb $API_KEY bitcoin 20000101 20171215 200
 ruby nytimes-article-api.rb $API_KEY google 20000101 20171215 200
-
-#####
-
-python train.py --rnn_size 256 --batch_size 200 --seq_length 200 --num_epoch 400 --learning_rate 0.001 --data_dir=../headlines-rnn/data/ # APPLE
+ruby nytimes-article-api.rb $API_KEY google 20110923 20171215 200
